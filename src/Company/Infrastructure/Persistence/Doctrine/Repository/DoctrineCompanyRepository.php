f86e2d78b0a6371ac22151383e8eb72d6bc0ec98
Marco Olivette <marco.vieira.olivette>
2020-06-03 20:49:22 -0300

    wip.

---
+++ src/Company/Infrastructure/Persistence/Doctrine/Repository/DoctrineCompanyRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Company\Infrastructure\Persistence\Doctrine\Repository;
+
+use App\Company\Domain\Entity\Company;
+use App\Company\Domain\Repository\CompanyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectRepository;
+
+class DoctrineCompanyRepository implements CompanyRepository
+{
+
+    /**
+     * @var ObjectRepository
+     */
+    private $repository;
+
+    public function __construct(
+        EntityManagerInterface $entityManager
+    )
+    {
+        $this->repository = $entityManager->getRepository(Company::class);
+    }
+
+    public function fromId(int $id)
+    {
+        // TODO: Implement fromId() method.
+    }
+
+    public function getAll()
+    {
+        return $this->repository->findAll();
+    }
+
+    public function getMother()
+    {
+        // TODO: Implement getMother() method.
+    }
+}
--- src/Company/Presentation/Http/Action/ListCompaniesAction.php
+++ src/Company/Presentation/Http/Action/ListCompaniesAction.php
@@ -4,12 +4,18 @@
 namespace App\Company\Presentation\Http\Action;
 
 
+use App\Company\Domain\Repository\CompanyRepository;
+use App\Company\Infrastructure\Persistence\Doctrine\Repository\DoctrineCompanyRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ListCompaniesAction
 {
+
     public function __invoke()
     {
-        return new JsonResponse([], 200);
+        $companyRepository = new DoctrineCompanyRepository();
+        $companies = $companyRepository->getAll();
+        dd($companies);
+        return new JsonResponse($companies, 200);
     }
 }
