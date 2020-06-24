8ccad0a72643f713910bdbcc5a1d3149b5278b73
Leonardo Rocha <lerooks.nick@gmail.com>
2020-06-24 19:45:06 -0300

    Add user fields

--- src/Security/Domain/Entity/User.php
+++ src/Security/Domain/Entity/User.php
@@ -5,6 +5,10 @@ namespace App\Security\Domain\Entity;
 use JsonSerializable;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+/**
+ * Class User
+ * @package App\Security\Domain\Entity
+ */
 class User implements UserInterface, JsonSerializable
 {
 
@@ -29,31 +33,69 @@ class User implements UserInterface, JsonSerializable
     private $role;
 
     /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var
+     */
+    private $birthdate;
+
+    /**
+     * @var boolean
+     */
+    private $active;
+
+    /**
+     * @var int
+     */
+    private $companyId;
+
+    /**
      * User constructor.
-     * @param int $id
+     * @param int|null $id
      * @param string $cpf
      * @param string $password
      * @param string $role
+     * @param string $email
+     * @param $birthdate
+     * @param bool $active
+     * @param int $companyId
      */
-    public function __construct(int $id, string $cpf, string $password, string $role)
+    public function __construct(?int $id, string $cpf, string $password, string $role, string $email, $birthdate, bool $active, int $companyId)
     {
         $this->id = $id;
         $this->cpf = $cpf;
         $this->password = $password;
         $this->role = $role;
+        $this->email = $email;
+        $this->birthdate = $birthdate;
+        $this->active = $active;
+        $this->companyId = $companyId;
     }
 
+    /**
+     * @return array|mixed
+     */
     public function jsonSerialize()
     {
         return $this->toArray();
     }
 
+    /**
+     * @return array
+     */
     public function toArray()
     {
         return [
             'id' => $this->getId(),
             'cpf' => $this->getCpf(),
-            'roles' => $this->getRoles()
+            'birthdate' => $this->getBirthdate(),
+            'email' => $this->getEmail(),
+            'companyId' => $this->getCompanyId(),
+            'isActive' => $this->isActive(),
+            'roles' => $this->getRoles(),
         ];
     }
 
@@ -73,16 +115,85 @@ class User implements UserInterface, JsonSerializable
         $this->id = $id;
     }
 
-    public function getCpf(): ?string
+    /**
+     * @return string
+     */
+    public function getCpf(): string
     {
         return $this->cpf;
     }
 
-    public function setCpf(string $cpf): self
+    /**
+     * @param string $cpf
+     */
+    public function setCpf(string $cpf): void
     {
         $this->cpf = $cpf;
+    }
 
-        return $this;
+    /**
+     * @return mixed
+     */
+    public function getBirthdate()
+    {
+        return $this->birthdate;
+    }
+
+    /**
+     * @param mixed $birthdate
+     */
+    public function setBirthdate($birthdate): void
+    {
+        $this->birthdate = $birthdate;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getCompanyId(): ?int
+    {
+        return $this->companyId;
+    }
+
+    /**
+     * @param int $companyId
+     */
+    public function setCompanyId(int $companyId): void
+    {
+        $this->companyId = $companyId;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    /**
+     * @param bool $active
+     */
+    public function setActive(bool $active): void
+    {
+        $this->active = $active;
     }
 
     /**
@@ -134,5 +245,21 @@ class User implements UserInterface, JsonSerializable
     public function setPassword(string $password): void
     {
         $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string $role
+     */
+    public function setRole(string $role): void
+    {
+        $this->role = $role;
     }
 }
