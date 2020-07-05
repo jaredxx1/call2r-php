152cf82a29f7b1b128ff4b8a662559c542eda7bd
Theundertow <marco.vieira.olivette@gmail.com>
2020-07-04 21:14:23 -0300

    fixing email

--- src/Core/Infrastructure/Container/Application/Exception/EmailSendException.php
+++ src/Core/Infrastructure/Container/Application/Exception/EmailSendException.php
@@ -7,9 +7,9 @@ namespace App\Core\Infrastructure\Container\Application\Exception;
 use Exception;
 use Throwable;
 
-class EmailException extends Exception
+class EmailSendException extends Exception
 {
-    public function __construct($message = "Error sending email", $code = 400, Throwable $previous = null)
+    public function __construct($message = "Error sending email", $code = 500, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
