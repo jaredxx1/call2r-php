becdb456d35aff4f418379ba2e5fd7279eec3369
Leonardo Rocha <lerooks.nick@gmail.com>
2020-08-08 10:54:14 -0300

    CORS config

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -32,3 +32,10 @@ $conn = [
 ];
 
 $entityManager = EntityManager::create($conn, $config);
+
+header ("Access-Control-Allow-Origin: *");
+header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
+header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+header ("Access-Control-Allow-Headers: *");
+header ("Access-Control-Request-Headers: *");
+header ("Access-Control-Request-Method: *");
