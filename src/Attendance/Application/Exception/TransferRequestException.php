47194b8799c178bf0de23134bad557d563d6c6f2
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-08-13 22:07:12 -0300

    .

--- src/Attendance/Application/Exception/TransferRequestException.php
+++ src/Attendance/Application/Exception/TransferRequestException.php
@@ -10,7 +10,7 @@ use Throwable;
 
 class TransferRequestException  extends Exception
 {
-    public function __construct($message = "You have must be assigned to the request to transfer it", $code = Response::HTTP_FORBIDDEN, Throwable $previous = null)
+    public function __construct($message = "You don't have permissions to transfer this request", $code = Response::HTTP_FORBIDDEN, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
