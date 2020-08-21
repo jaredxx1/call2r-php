45febaa3c315d018298e8429b5c1a501e91c2ad3
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-08-21 09:13:40 -0300

    .

--- src/Attendance/Application/Command/SubmitForApprovalCommand.php
+++ src/Attendance/Application/Command/SubmitForApprovalCommand.php
@@ -55,12 +55,14 @@ class SubmitForApprovalCommand implements CommandInterface
             Assert::stringNotEmpty($data['message'], 'Field message cannot be empty.');
         }
 
-        self::validateRequest($data['request'], $data['user']);
+        if(key_exists('request', $data) && (key_exists('user', $data))){
+            self::validateRequest($data['request'], $data['user']);
+        }
 
         return new self(
             $data['message'] ?? null,
-            $data[],
-            $data[]
+            $data['request'],
+            $data['user']
         );
     }
 
