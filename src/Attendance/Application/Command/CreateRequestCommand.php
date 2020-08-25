a771fdfa4da5bdd8a342d5a30b53217d6bb54235
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-08-24 23:32:50 -0300

    Calculate SLA complete.

--- src/Attendance/Application/Command/CreateRequestCommand.php
+++ src/Attendance/Application/Command/CreateRequestCommand.php
@@ -73,7 +73,6 @@ class CreateRequestCommand implements CommandInterface
         Assert::integer($data['priority'], ' Field priority is not an integer');
         Assert::integer($data['sectionId'], ' Field sectionId is not a string');
 
-        Assert::stringNotEmpty($data['sectionId'], 'Field sectionId is empty');
         Assert::stringNotEmpty($data['title'], 'Field title is empty');
         Assert::stringNotEmpty($data['description'], 'Field description is empty');
         Assert::stringNotEmpty($data['section'], 'Field section is empty');
